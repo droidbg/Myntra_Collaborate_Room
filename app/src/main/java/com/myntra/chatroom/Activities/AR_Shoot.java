9b0198f88bd1a72843a7382d92cf62e5b422fb83
Binni Goel <binni1108@gmail.com>
2021-04-04 11:27:53 +0530

    Myntra Chatroom 95% ready, the only thing left now is to add AR to the camera, Left work in this chat room app: Photo is going and saving but clicking and viewing photo is left

---
+++ app/src/main/java/com/myntra/chatroom/Activities/AR_Shoot.java
@@ -0,0 +1,14 @@
+package com.myntra.chatroom.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AR_Shoot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_r__shoot);
+    }
+}
